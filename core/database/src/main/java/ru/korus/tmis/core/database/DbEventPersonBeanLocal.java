f9cf149bd08ef9efa8a7c98945e18c5154881517
mmakankov <mmakankov@gmail.com>
2012-12-26 12:26:40 +0400

    - [fix] Версия 1.0.0.55

--- core/database/src/main/java/ru/korus/tmis/core/database/DbEventPersonBeanLocal.java
+++ core/database/src/main/java/ru/korus/tmis/core/database/DbEventPersonBeanLocal.java
@@ -1,5 +1,10 @@
 package ru.korus.tmis.core.database;
 
+import ru.korus.tmis.core.entity.model.EventPerson;
+import ru.korus.tmis.core.exception.CoreException;
+
+import javax.ejb.Local;
+
 /**
  * Created with IntelliJ IDEA.
  * User: mmakankov
@@ -7,5 +12,14 @@ package ru.korus.tmis.core.database;
  * Time: 16:15
  * To change this template use File | Settings | File Templates.
  */
+
+@Local
 public interface DbEventPersonBeanLocal {
+    /**
+     * Запрос на тип EventPerson по идентификатору.
+     * @param id идентификатор записи.
+     * @return
+     * @throws CoreException
+     */
+    EventPerson getEventPersonById(int id) throws CoreException;
 }
--- core/database/src/main/scala/ru/korus/tmis/core/database/DbEventPersonBean.scala
+++ core/database/src/main/scala/ru/korus/tmis/core/database/DbEventPersonBean.scala
@@ -3,13 +3,14 @@ package ru.korus.tmis.core.database
 import grizzled.slf4j.Logging
 import ru.korus.tmis.util.{ConfigManager, I18nable}
 import javax.persistence.{EntityManager, PersistenceContext}
-import ru.korus.tmis.core.entity.model.EventPerson
+import ru.korus.tmis.core.entity.model.{Event, Staff, EventPerson}
 import ru.korus.tmis.core.exception.CoreException
 import scala.collection.JavaConversions._
 import ru.korus.tmis.core.data.{QuotaTypesListRequestDataFilter, DictionaryListRequestDataFilter, QueryDataStructure}
 import javax.interceptor.Interceptors
 import ru.korus.tmis.core.logging.LoggingInterceptor
 import javax.ejb.Stateless
+import java.util.Date
 
 /**
  * Класс с методами для работы с таблицей s11r64.Event_Persons
@@ -26,6 +27,36 @@ class DbEventPersonBean
 
   @PersistenceContext(unitName = "s11r64")
   var em: EntityManager = _
+
+  def insertOrUpdateEventPerson(id: Int,
+                                event: Event,
+                                sessionUser: Staff): EventPerson = {
+    var ep: EventPerson = null
+    val now = new Date
+    if (id > 0) {
+      ep = getEventPersonById(id)
+    }
+    else {
+      ep = new EventPerson
+      ep.setPerson(sessionUser)
+      if (event != null) {
+        ep.setEvent(event)
+        ep.setBegDate(event.getCreateDatetime)
+      }
+    }
+
+    //ep.setDeleted(false)
+    ep
+  }
+
+  def deleteEventPerson(id: Int,
+                        sessionUser: Staff) = {
+    val d = getClientRelationById(id)
+    val now = new Date
+    d.setDeleted(true)
+    d.setModifyPerson(sessionUser)
+    d.setModifyDatetime(now)
+  }
 
   def getEventPersonById(id: Int) = {
     val query = em.createQuery(EventPersonFindQuery,
