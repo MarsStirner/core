ccfd959b1bf19f124a710303cb274ece0e6b5641
NosovDE <NosovDE@gmail.com>
2013-02-04 13:11:37 +0400

    - Fix для трансфузиологии

--- core/ws-transfusion/src/test/java/ru/kous/tmis/ws/transfusion/TestTransfusionMedicalService.java
+++ core/ws-transfusion/src/test/java/ru/kous/tmis/ws/transfusion/TestTransfusionMedicalService.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 import org.junit.Test;
 
-import ru.korus.tmis.ws.transfusion.ProcedureType;
-import ru.korus.tmis.ws.transfusion.TransfusionMedicalService;
-import ru.korus.tmis.ws.transfusion.TransfusionMedicalService_Service;
+//import ru.korus.tmis.ws.transfusion.ProcedureType;
+//import ru.korus.tmis.ws.transfusion.TransfusionMedicalService;
+//import ru.korus.tmis.ws.transfusion.TransfusionMedicalService_Service;
 
 import static org.junit.Assert.assertTrue;
 
@@ -19,16 +19,16 @@ import static org.junit.Assert.assertTrue;
 public class TestTransfusionMedicalService {
    // @Test
     public void test() {
-        TransfusionMedicalService_Service service = new TransfusionMedicalService_Service();
-        
-        final TransfusionMedicalService transfusionMedicalService = service.getTransfusionMedicalService();
-        List<ProcedureType> res =
-                transfusionMedicalService.getProcedureTypes();
-        if(res != null) {
-            for(ProcedureType pt : res)
-                System.out.println("id: " + pt.getId() + " value: " + pt.getValue());
-        }
-        assertTrue(true);
+//        TransfusionMedicalService_Service service = new TransfusionMedicalService_Service();
+//
+//        final TransfusionMedicalService transfusionMedicalService = service.getTransfusionMedicalService();
+//        List<ProcedureType> res =
+//                transfusionMedicalService.getProcedureTypes();
+//        if(res != null) {
+//            for(ProcedureType pt : res)
+//                System.out.println("id: " + pt.getId() + " value: " + pt.getValue());
+//        }
+//        assertTrue(true);
             
     }
 
