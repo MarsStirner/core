7119b520c330e4527080890b8860ee43179b0816
NosovDE <NosovDE@gmail.com>
2013-02-13 16:19:34 +0400

    - Fix

--- core/server/scripts/create-domain.sh
+++ core/server/scripts/create-domain.sh
@@ -35,42 +35,44 @@ asadmin start-domain --domaindir ${glassfish.domain.dir} ${glassfish.domain}
 # Прописываем конфигурацию логера
 asadmin --user ${glassfish.admin.login} \
         --passwordfile $GF_PASSWD_FILE \
-        create-jvm-options --target server-config --'-Dlogback.configurationFile=${com.sun.aas.instanceRoot}/config/logback.xml'
+        create-jvm-options --target server-config '-Dlogback.configurationFile=${com.sun.aas.instanceRoot}/config/logback.xml'
 asadmin --user ${glassfish.admin.login} \
         --passwordfile $GF_PASSWD_FILE \
-        create-jvm-options --target default-config --'-Dlogback.configurationFile=${com.sun.aas.instanceRoot}/config/logback.xml'
+        create-jvm-options --target default-config '-Dlogback.configurationFile=${com.sun.aas.instanceRoot}/config/logback.xml'
 
 #Настройки JVM
 asadmin --user ${glassfish.admin.login} \
         --passwordfile $GF_PASSWD_FILE \
-        create-jvm-options --target server-config --'-XX\:+UnlockExperimentalVMOptions'
+        create-jvm-options --target server-config '-XX\:+UnlockExperimentalVMOptions'
 asadmin --user ${glassfish.admin.login} \
         --passwordfile $GF_PASSWD_FILE \
-        delete-jvm-options --target server-config --'-Xmx512m'
+        delete-jvm-options --target server-config '-Xmx512m'
 asadmin --user ${glassfish.admin.login} \
         --passwordfile $GF_PASSWD_FILE \
-        create-jvm-options --target server-config --'-Xmx1024m'
+        create-jvm-options --target server-config '-Xmx1024m'
 asadmin --user ${glassfish.admin.login} \
         --passwordfile $GF_PASSWD_FILE \
-        delete-jvm-options --target server-config --'-XX\:MaxPermSize=192m'
+        delete-jvm-options --target server-config '-XX\:MaxPermSize=192m'
 asadmin --user ${glassfish.admin.login} \
         --passwordfile $GF_PASSWD_FILE \
-        create-jvm-options --target server-config --'-XX\:MaxPermSize=256m'
+        create-jvm-options --target server-config '-XX\:MaxPermSize=256m'
+
+
 asadmin --user ${glassfish.admin.login} \
         --passwordfile $GF_PASSWD_FILE \
-        create-jvm-options --target default-config --'-XX\:+UnlockExperimentalVMOptions'
+        create-jvm-options --target default-config '-XX\:+UnlockExperimentalVMOptions'
 asadmin --user ${glassfish.admin.login} \
         --passwordfile $GF_PASSWD_FILE \
-        delete-jvm-options --target default-config --'-Xmx512m'
+        delete-jvm-options --target default-config '-Xmx512m'
 asadmin --user ${glassfish.admin.login} \
         --passwordfile $GF_PASSWD_FILE \
-        create-jvm-options --target default-config --'-Xmx1024m'
+        create-jvm-options --target default-config '-Xmx1024m'
 asadmin --user ${glassfish.admin.login} \
         --passwordfile $GF_PASSWD_FILE \
-        delete-jvm-options --target default-config --'-XX\:MaxPermSize=192m'
+        delete-jvm-options --target default-config '-XX\:MaxPermSize=192m'
 asadmin --user ${glassfish.admin.login} \
         --passwordfile $GF_PASSWD_FILE \
-        create-jvm-options --target default-config --'-XX\:MaxPermSize=256m'
+        create-jvm-options --target default-config '-XX\:MaxPermSize=256m'
 
 
 # Создание пулов соединений и ресурсов для необходимых ядру БД
@@ -80,11 +82,11 @@ asadmin --user ${glassfish.admin.login} \
         --datasourceclassname com.mysql.jdbc.jdbc2.optional.MysqlDataSource \
         --property user=${mysql.login}:password=${mysql.password}:DatabaseName=${mysql.db.mis}:ServerName=${mysql.db.host}:port=${mysql.db.port}:useUnicode=true:characterEncoding=UTF-8:characterSetResults=UTF-8:datasourceName=${mysql.db.mis}:zeroDateTimeBehavior=convertToNull \
         --validateatmostonceperiod=60 --creationretryattempts=3 --creationretryinterval=10 --isconnectvalidatereq=true --validationmethod=auto-commit --ping=true \
-        ${mysql.db.mis}-pool
+        ${mysql.db.mis.pool}
 asadmin --user ${glassfish.admin.login} \
         --passwordfile $GF_PASSWD_FILE \
         create-jdbc-resource \
-        --connectionpoolid ${mysql.db.mis}-pool ${mysql.db.mis}
+        --connectionpoolid ${mysql.db.mis.pool} ${mysql.db.mis}
 
 
 asadmin --user ${glassfish.admin.login} \
@@ -93,11 +95,11 @@ asadmin --user ${glassfish.admin.login} \
         --datasourceclassname com.mysql.jdbc.jdbc2.optional.MysqlDataSource \
         --property user=${mysql.login}:password=${mysql.password}:DatabaseName=${mysql.db.rls}:ServerName=${mysql.db.host}:port=${mysql.db.port}:useUnicode=true:characterEncoding=UTF-8:characterSetResults=UTF-8:datasourceName=${mysql.db.rls}:zeroDateTimeBehavior=convertToNull \
         --validateatmostonceperiod=60 --creationretryattempts=3 --creationretryinterval=10 --isconnectvalidatereq=true --validationmethod=auto-commit --ping=true \
-        ${mysql.db.rls}-pool
+        ${mysql.db.rls.pool}
 asadmin --user ${glassfish.admin.login} \
         --passwordfile $GF_PASSWD_FILE \
         create-jdbc-resource \
-        --connectionpoolid ${mysql.db.rls}-pool ${mysql.db.rls}
+        --connectionpoolid ${mysql.db.rls.pool} ${mysql.db.rls}
 
 
 asadmin --user ${glassfish.admin.login} \
@@ -106,11 +108,11 @@ asadmin --user ${glassfish.admin.login} \
         --datasourceclassname com.mysql.jdbc.jdbc2.optional.MysqlDataSource \
         --property user=${mysql.login}:password=${mysql.password}:DatabaseName=${mysql.db.tmis_core}:ServerName=${mysql.db.host}:port=${mysql.db.port}:useUnicode=true:characterEncoding=UTF-8:characterSetResults=UTF-8:datasourceName=${mysql.db.tmis_core}:zeroDateTimeBehavior=convertToNull \
         --validateatmostonceperiod=60 --creationretryattempts=3 --creationretryinterval=10 --isconnectvalidatereq=true --validationmethod=auto-commit --ping=true \
-        ${mysql.db.tmis_core}-pool
+        ${mysql.db.tmis_core.pool}
 asadmin --user ${glassfish.admin.login} \
         --passwordfile $GF_PASSWD_FILE \
         create-jdbc-resource \
-        --connectionpoolid ${mysql.db.tmis_core}-pool ${mysql.db.tmis_core}
+        --connectionpoolid ${mysql.db.tmis_core.pool} ${mysql.db.tmis_core}
 
 rm -f $GF_PASSWD_FILE
 
