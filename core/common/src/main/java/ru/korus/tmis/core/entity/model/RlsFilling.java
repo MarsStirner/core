4e8764296c4d9926c17c20d1c351f4bb8657d152
szagrebelny <szgrebelny@korusconsulting.ru>
2013-06-06 12:24:49 +0400

    add rls entity

---
+++ core/common/src/main/java/ru/korus/tmis/core/entity/model/RlsFilling.java
@@ -0,0 +1,54 @@
+package ru.korus.tmis.core.entity.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the rlsFilling database table.
+ * 
+ */
+@Entity
+@Table(name="rlsFilling")
+public class RlsFilling implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(unique=true, nullable=false)
+	private int id;
+
+	@Column(nullable=false)
+	private boolean disabledForPrescription;
+
+	@Column(length=128)
+	private String name;
+
+	public RlsFilling() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public boolean getDisabledForPrescription() {
+		return this.disabledForPrescription;
+	}
+
+	public void setDisabledForPrescription(boolean disabledForPrescription) {
+		this.disabledForPrescription = disabledForPrescription;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
