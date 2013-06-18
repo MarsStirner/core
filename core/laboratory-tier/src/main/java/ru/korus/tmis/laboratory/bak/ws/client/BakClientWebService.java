87e0e9555be9d06a2922627de51d3b001d1df61e
anosov <anosov@outlook.com>
2013-06-18 18:18:45 +0400

    added client

---
+++ core/laboratory-tier/src/main/java/ru/korus/tmis/laboratory/bak/ws/client/BakClientWebService.java
@@ -0,0 +1,29 @@
+package ru.korus.tmis.laboratory.bak.ws.client;
+
+import ru.korus.tmis.core.exception.CoreException;
+import ru.korus.tmis.laboratory.bak.bean.BakLaboratoryBeanLocal;
+
+import javax.ejb.EJB;
+import javax.jws.WebService;
+
+/**
+ * @author anosov
+ *         Date: 18.06.13 18:12
+ */
+
+@WebService(
+  endpointInterface = "ru.korus.tmis.laboratory.bak.ws.client.IBakClientWebService",
+  targetNamespace = "http://korus.ru/tmis/client-bak",
+  serviceName = "tmis-client-bak",
+  portName = "client-bak",
+  name = "client-bak")
+public class BakClientWebService implements IBakClientWebService{
+
+    @EJB
+    BakLaboratoryBeanLocal bakLaboratoryBeanLocal;
+
+    @Override
+    public void sendAnalysisRequest(int actionId) throws CoreException {
+        bakLaboratoryBeanLocal.sendLisAnalysisRequest();
+    }
+}
