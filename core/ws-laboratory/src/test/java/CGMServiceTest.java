0cdc0e8598112be7d2bce8c12633b4efd6559a4f
anosov <anosov@outlook.com>
2013-07-26 13:04:13 +0400

    comment tests in  CGMServiceTest.java

--- core/ws-laboratory/src/test/java/CGMServiceTest.java
+++ core/ws-laboratory/src/test/java/CGMServiceTest.java
@@ -1,16 +1,11 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.cgm.*;
 
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
 import javax.xml.bind.Marshaller;
 import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
 import java.io.IOException;
 import java.io.StringWriter;
-import java.util.GregorianCalendar;
 
 /**
  * Author:      Dmitriy E. Nosov <br>
@@ -24,60 +19,60 @@ public class CGMServiceTest {
 
     public static void main(String[] args) throws DatatypeConfigurationException {
 
-        final CGMSOAP cgmsoap = new CGMSOAP();
-        final ObjectFactory obj = new ObjectFactory();
-        final HL7Document hl7Document = obj.createHL7Document();
-
-        hl7Document.setId("2222");
-        hl7Document.setCode("11122");
-        hl7Document.setSetID("333311122");
-        hl7Document.setTitle("title");
-        hl7Document.setTypeId("typeid");
-        hl7Document.setVersionNumber(111);
-        hl7Document.setConfidentialityCode("3333");
-
-        final ComponentInfo componentInfo = obj.createComponentInfo();
-        hl7Document.setComponent(componentInfo);
-
-        final ComponentOfInfo componentOfInfo = obj.createComponentOfInfo();
-        hl7Document.setComponentOf(componentOfInfo);
-
-        final CustodianInfo custodianInfo = obj.createCustodianInfo();
-        hl7Document.setCustodian(custodianInfo);
-
-        final RecordTargetInfo recordTargetInfo = obj.createRecordTargetInfo();
-        hl7Document.setRecordTarget(recordTargetInfo);
-
-        final GregorianCalendar gcal = (GregorianCalendar) GregorianCalendar.getInstance();
-        final XMLGregorianCalendar xgcal = DatatypeFactory.newInstance().newXMLGregorianCalendar(gcal);
-        hl7Document.setEffectiveTime(xgcal);
-
-
-        final AuthorInfo authorInfo = obj.createAuthorInfo();
-        final AssignedAuthorInfo assignedAuthorInfo = obj.createAssignedAuthorInfo();
-        assignedAuthorInfo.setId(1);
-        final AssignedPersonInfo assignedPersonInfo = obj.createAssignedPersonInfo();
-
-        final JAXBElement<String> family = obj.createAssignedPersonInfoGiven("Family");
-        final JAXBElement<String> name = obj.createAssignedPersonInfoGiven("Name");
-        final JAXBElement<String> patr = obj.createAssignedPersonInfoGiven("Patr");
-
-        assignedPersonInfo.getContent().add(family);
-        assignedPersonInfo.getContent().add(name);
-        assignedPersonInfo.getContent().add(patr);
-
-
-        assignedAuthorInfo.setAssignedPerson(assignedPersonInfo);
-        authorInfo.setAssignedAuthor(assignedAuthorInfo);
-        hl7Document.setAuthor(authorInfo);
-
-        final PatientInfo patientInfo = obj.createPatientInfo();
-
-        System.out.println("start = " + marshallMessage(hl7Document, "ru.cgm"));
-
-        final int result = cgmsoap.getCgmsoapPortType().queryAnalysis(hl7Document);
-
-        System.out.println("result = " + result);
+//        final CGMSOAP cgmsoap = new CGMSOAP();
+//        final ObjectFactory obj = new ObjectFactory();
+//        final HL7Document hl7Document = obj.createHL7Document();
+//
+//        hl7Document.setId("2222");
+//        hl7Document.setCode("11122");
+//        hl7Document.setSetID("333311122");
+//        hl7Document.setTitle("title");
+//        hl7Document.setTypeId("typeid");
+//        hl7Document.setVersionNumber(111);
+//        hl7Document.setConfidentialityCode("3333");
+//
+//        final ComponentInfo componentInfo = obj.createComponentInfo();
+//        hl7Document.setComponent(componentInfo);
+//
+//        final ComponentOfInfo componentOfInfo = obj.createComponentOfInfo();
+//        hl7Document.setComponentOf(componentOfInfo);
+//
+//        final CustodianInfo custodianInfo = obj.createCustodianInfo();
+//        hl7Document.setCustodian(custodianInfo);
+//
+//        final RecordTargetInfo recordTargetInfo = obj.createRecordTargetInfo();
+//        hl7Document.setRecordTarget(recordTargetInfo);
+//
+//        final GregorianCalendar gcal = (GregorianCalendar) GregorianCalendar.getInstance();
+//        final XMLGregorianCalendar xgcal = DatatypeFactory.newInstance().newXMLGregorianCalendar(gcal);
+//        hl7Document.setEffectiveTime(xgcal);
+//
+//
+//        final AuthorInfo authorInfo = obj.createAuthorInfo();
+//        final AssignedAuthorInfo assignedAuthorInfo = obj.createAssignedAuthorInfo();
+//        assignedAuthorInfo.setId(1);
+//        final AssignedPersonInfo assignedPersonInfo = obj.createAssignedPersonInfo();
+//
+//        final JAXBElement<String> family = obj.createAssignedPersonInfoGiven("Family");
+//        final JAXBElement<String> name = obj.createAssignedPersonInfoGiven("Name");
+//        final JAXBElement<String> patr = obj.createAssignedPersonInfoGiven("Patr");
+//
+//        assignedPersonInfo.getContent().add(family);
+//        assignedPersonInfo.getContent().add(name);
+//        assignedPersonInfo.getContent().add(patr);
+//
+//
+//        assignedAuthorInfo.setAssignedPerson(assignedPersonInfo);
+//        authorInfo.setAssignedAuthor(assignedAuthorInfo);
+//        hl7Document.setAuthor(authorInfo);
+//
+//        final PatientInfo patientInfo = obj.createPatientInfo();
+//
+//        System.out.println("start = " + marshallMessage(hl7Document, "ru.cgm"));
+//
+//        final int result = cgmsoap.getCgmsoapPortType().queryAnalysis(hl7Document);
+//
+//        System.out.println("result = " + result);
 
     }
 
