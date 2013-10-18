9c0c5557f3f08b890c4d15031e1bfe80a217d976
AKislin <AKislin@korusconsulting.ru>
2013-10-18 13:25:02 +0400

    upgrade create domain and deployment scripts

--- core/server/scripts/unix-create-domain.sh
+++ core/server/scripts/unix-create-domain.sh
@@ -31,39 +31,9 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 GF_PASSWD_FILE="${DIR}/password"
 
-ASADMIN=${glassfish.bin}
-DOMAIN=${glassfish.domain}
-DOMAIN_DIR=${glassfish.domain.dir}
-ADMIN_LOGIN=${glassfish.admin.login}
-ADMIN_PASSWORD=${glassfish.admin.password}
-PORT_INSTANCE=${glassfish.port.instance}
-ADMIN_PORT=${glassfish.port.admin}
-MASTER_PASSWORD=${glassfish.master.password}
-
-MYSQL_LOGIN=${mysql.login}
-MYSQL_PASSWORD=${mysql.password}
-MYSQL_SERVER_NAME=${mysql.db.host}
-MYSQL_PORT=${mysql.db.port}
-
-MYSQL_DB_MIS=${mysql.db.mis}
-MYSQL_DB_MIS_POOL=${mysql.db.mis.pool}
-MYSQL_DB_JNDI_MIS=${mysql.db.jndi.mis}
-
-MYSQL_DB_RLS=${mysql.db.rls}
-MYSQL_DB_RLS_POOL=${mysql.db.rls.pool}
-MYSQL_DB_JNDI_RLS=${mysql.db.jndi.rls}
-
-MYSQL_DB_TMIS_CORE=${mysql.db.tmis_core}
-MYSQL_DB_TMIS_CORE_POOL=${mysql.db.tmis_core.pool}
-MYSQL_DB_JNDI_TMIS_CORE=${mysql.db.jndi.tmis_core}
-
-COPY_CONFIG_CMD="cp ${DIR}/logback.xml ${DOMAIN_DIR}/${DOMAIN}/config"
+source ${DIR}/unix-common.sh
 
 COPY_LIBRARIES_CMD="cp ${DIR}/../scripts/lib/* ${DOMAIN_DIR}/${DOMAIN}/lib/ext/"
-
-LIST_DOMAINS_CMD="${ASADMIN}/asadmin list-domains"
-
-STOP_DOMAIN_CMD="${ASADMIN}/asadmin stop-domain ${DOMAIN}"
 
 DELETE_DOMAIN_CMD="${ASADMIN}/asadmin delete-domain ${DOMAIN}"
 
@@ -76,27 +46,12 @@ CREATE_DOMAIN_CMD="${ASADMIN}/asadmin --user ${ADMIN_LOGIN} --passwordfile $GF_P
         --domaindir ${DOMAIN_DIR} \
         ${DOMAIN}"
 
-START_DOMAIN_CMD="${ASADMIN}/asadmin start-domain
-        --passwordfile $GF_PASSWD_FILE \
-        --domaindir ${DOMAIN_DIR} \
-         ${DOMAIN}"
-
 ENABLE_SECURE_ADMIN_CMD="${ASADMIN}/asadmin --user ${ADMIN_LOGIN} \
         --passwordfile $GF_PASSWD_FILE \
         --port ${ADMIN_PORT} \
         enable-secure-admin"
 
 
-function list_domains {
-    print_header "List domains"
-
-    echo $LIST_DOMAINS_CMD
-    if ! $LIST_DOMAINS_CMD; then
-        # Если получение списка доменов завершается ошибкой, то продолжать смысла нет, так как это говорит о серьезной
-        # проблеме, например неправильно заданной директории GlassFish
-        exit 1
-    fi
-}
 
 function stop_domain {
     print_header "Stop domain ${DOMAIN}"
@@ -144,15 +99,6 @@ function copy_config_files {
     fi
 }
 
-function start_domain {
-    print_header "Start domain ${DOMAIN}"
-
-    echo $START_DOMAIN_CMD
-    if ! $START_DOMAIN_CMD; then
-        # Ошибка запуска домена критична
-        exit 4
-    fi
-}
 
 function enable_secure_admin {
     print_header "Enable secure admin"
@@ -250,19 +196,6 @@ function start_stop_domain {
     $START_DOMAIN_CMD
     print_separator
 
-}
-
-function print_separator {
-    columns=$(tput cols)
-    for ((x = 0; x < columns; x++)); do
-        printf %s -
-    done
-}
-
-function print_header {
-    print_separator
-    echo "$(tput setaf 2)$1$(tput sgr0)"
-    echo ""
 }
 
 # Удаление файла с паролями по завершению скрипта (в том числе при возникновении ошибки)
