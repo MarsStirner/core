edc618afea1338ae7aab7afa51eb6f77155efd53
anosov <anosov@outlook.com>
2013-10-21 17:36:57 +0400

    update risar

--- core/ws-laboratory/src/main/java/ru/risar/data/RegistrationPatientResponse.java
+++ core/ws-laboratory/src/main/java/ru/risar/data/RegistrationPatientResponse.java
@@ -12,7 +12,7 @@ import javax.xml.bind.annotation.XmlType;
 
 
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "registrationPatientResponse", propOrder = {
+@XmlType(name = "RegistrationPatientResponse", propOrder = {
         "code",
         "description"
 })
