5b5eae253a8628d710bb55214d24a6a7cf0c37ab
MarmaladeSky <alexey.kislin@gmail.com>
2014-01-30 16:47:48 +0400

    add getContractsByEventTypeId method

--- core/database/src/main/java/ru/korus/tmis/core/database/common/DbContractBeanLocal.java
+++ core/database/src/main/java/ru/korus/tmis/core/database/common/DbContractBeanLocal.java
@@ -5,6 +5,7 @@ import ru.korus.tmis.core.entity.model.EventType;
 import ru.korus.tmis.core.exception.CoreException;
 
 import javax.ejb.Local;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,5 +19,14 @@ public interface DbContractBeanLocal {
 
     Contract getContractById(int id) throws CoreException;
 
+    /**
+     * Поведение данного метода неверно - контрактов для одного типа событий
+     * может быть несколько. Используйте {@link #getContractsByEventTypeId(int, int, boolean, boolean) getContractsByEventTypeId}
+     * @param eventType Тип события
+     * @return Первый контракт из списка или null, если таковых не нашлось
+     */
+    @Deprecated
     Contract getContractForEventType(EventType eventType);
+
+    List<Contract> getContractsByEventTypeId(int eventTypeId, int financeId, boolean showDeleted, boolean showExpired);
 }
