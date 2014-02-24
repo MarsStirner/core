95376827bf26175d2edd9037640de149a6b49b67
szagrebelny <szgrebelny@korusconsulting.ru>
2014-02-24 16:22:37 +0400

    HS integration: request patient info

---
+++ core/referencebook/src/main/java/ru/korus/tmis/ehr/CallbackService.java
@@ -0,0 +1,70 @@
+package ru.korus.tmis.ehr;
+
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Element;
+import ru.korus.tmis.ehr.ws.callback.BaseResponse;
+import ru.korus.tmis.ehr.ws.callback.DocumentResponse;
+import ru.korus.tmis.ehr.ws.callback.PatientResponse;
+import ru.korus.tmis.ehr.ws.callback.RetrieveDocumentResponse;
+
+import javax.annotation.Resource;
+import javax.jws.WebService;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+import java.util.List;
+
+/**
+ * Author:      Sergey A. Zagrebelny <br>
+ * Date:        20.02.14, 15:55 <br>
+ * Company:     Korus Consulting IT<br>
+ * Description:  <br>
+ */
+@WebService(serviceName = "CallbackService", portName = "CallbackServiceSoap", endpointInterface = "ru.korus.tmis.ehr.ws.callback.CallbackServiceSoap", targetNamespace = "urn:wsdl", wsdlLocation = "WEB-INF/wsdl/CallbackService.wsdl")
+public class CallbackService {
+
+    private static final Logger logger = LoggerFactory.getLogger(CallbackService.class);
+
+    private static RestCallback restCallback = null;
+
+    @Resource
+    private WebServiceContext context;
+
+    public void containerResponse(BaseResponse parameters) {
+        logger.info("CallbackService.containerResponse: parameters = " + (new Gson()).toJson(parameters) );
+        List<Element> list =
+                (List<Element>) context.getMessageContext().get(MessageContext.REFERENCE_PARAMETERS);
+        if (restCallback != null) {
+            restCallback.containerResponse(parameters);
+        }
+    }
+
+    public void patientQueryResponse(PatientResponse parameters) {
+        logger.info("CallbackService.patientQueryResponse: parameters = " + (new Gson()).toJson(parameters) );
+
+        if (restCallback != null) {
+            restCallback.patientQueryResponse(parameters);
+        }
+    }
+
+    public void documentQueryResponse(DocumentResponse parameters) {
+        logger.info("CallbackService.documentQueryResponse: parameters = " + (new Gson()).toJson(parameters) );
+        if (restCallback != null) {
+            restCallback.documentQueryResponse(parameters);
+        }
+    }
+
+    public void retrieveDocumentQueryResponse(RetrieveDocumentResponse parameters) {
+        logger.info("CallbackService.retrieveDocumentQueryResponse: parameters = " + (new Gson()).toJson(parameters) );
+        if (restCallback != null) {
+            restCallback.retrieveDocumentQueryResponse(parameters);
+        }
+    }
+
+    public static void setRestCallback(RestCallback restCallback) {
+        CallbackService.restCallback = restCallback;
+    }
+
+
+}
---
+++ core/referencebook/src/main/java/ru/korus/tmis/ehr/EhrRest.java
@@ -0,0 +1,297 @@
+package ru.korus.tmis.ehr;
+
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.korus.tmis.ehr.ws.*;
+import ru.korus.tmis.ehr.ws.callback.BaseResponse;
+import ru.korus.tmis.ehr.ws.callback.DocumentResponse;
+import ru.korus.tmis.ehr.ws.callback.PatientResponse;
+import ru.korus.tmis.ehr.ws.callback.RetrieveDocumentResponse;
+import ru.korus.tmis.pix.sda.SdaHandlerResolver;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import java.util.UUID;
+
+/**
+ * Author:      Sergey A. Zagrebelny <br>
+ * Date:        20.02.14, 14:34 <br>
+ * Company:     Korus Consulting IT<br>
+ * Description:  <br>
+ */
+@Path("/api")
+public class EhrRest implements RestCallback {
+
+    private static final Logger logger = LoggerFactory.getLogger(EhrRest.class);
+
+    private final static ru.korus.tmis.ehr.ws.ObjectFactory EhrFactory = new ru.korus.tmis.ehr.ws.ObjectFactory();
+    private final static ru.korus.tmis.ehr.ws.callback.ObjectFactory CallbackFactory = new ru.korus.tmis.ehr.ws.callback.ObjectFactory();
+
+    private final Object lockPatientResponse = new Object();
+    private PatientResponse patientResponse = null;
+
+    private static class PatientQueryDoc {
+        private String series = null;
+        private String type = null;
+        private String number = null;
+    }
+
+    private static class PatientQueryInp {
+
+        private PatientQueryHeaders headers = null;
+
+        private static class PatientQueryHeaders {
+
+            private String BinarySecurityToken = null;
+        }
+
+        private PatientQueryData data = null;
+
+        private static class PatientQueryData {
+
+            private String facilityCode = null;
+            private PatientQueryInitiatedBy initiatedBy = null;
+
+            private static class PatientQueryInitiatedBy {
+
+                private PatientQuerySpeciality specialty = null;
+
+                private static class PatientQuerySpeciality {
+                    private String codingSystem = null;
+                    private String code = null;
+                    private String name = null;
+                }
+
+                private PatientQueryRole role;
+
+                private static class PatientQueryRole {
+                    private String codingSystem = null;
+                    private String code = null;
+                    private String name = null;
+                }
+
+                private String snils = null;
+                private String familyName = null;
+                private String givenName = null;
+                private String middleName = null;
+
+            }
+
+            private PatientQueryParams params = null;
+
+            private static class PatientQueryParams {
+                private String gender = null;
+                private String dobLow = null;
+                private String dobHigh = null;
+                private PatientQueryMrn mrn = null;
+
+                private static class PatientQueryMrn {
+                    private String root = null;
+                    private String extension = null;
+                }
+
+                private String familyName = null;
+                private String givenName = null;
+                private String middleName = null;
+                private String snils = null;
+                private PatientQueryDoc omsPolicy = null;
+                private PatientQueryDoc identityDocument = null;
+            }
+
+        }
+    }
+
+    @POST
+    @Consumes("application/json")
+    @Path("/patientQuery/")
+    public Response patientQuery(String input) {
+        //{"headers": {"BinarySecurityToken": "UmVlOWVldGk="},
+        // "data":{
+        //   "facilityCode": "1.2.643.5.1.13.3.25.58.57",
+        //   "initiatedBy": {
+        //      "specialty": {
+        //         "codingSystem": "1.2.643.5.1.13.2.1.1.181",
+        //         "code": "1135",
+        //         "name": "\u0414\u0435\u0442\u0441\u043a\u0430\u044f \u0445\u0438\u0440\u0443\u0440\u0433\u0438\u044f"
+        //      },
+        //      "role": {
+        //         "codingSystem": "1.2.643.5.1.13.2.1.1.607",
+        //         "code": "23",
+        //         "name": "\u0432\u0440\u0430\u0447 - \u0434\u0435\u0442\u0441\u043a\u0438\u0439 \u0445\u0438\u0440\u0443\u0440\u0433"},
+        //         "snils": "128-971-396 05 ",
+        //         "familyName": "\u0423\u0448\u0430\u043a\u043e\u0432",
+        //         "givenName": "\u0415\u0432\u0433\u0435\u043d\u0438\u0439",
+        //         "middleName": "\u0410\u043d\u0434\u0440\u0435\u0435\u0432\u0438\u0447"
+        //       },
+        //       "params": {
+        //         "gender": "M",
+        //         "dobLow": "1950-01-01",
+        //         "dobHigh": "1950-01-01",
+        //         "mrn": {
+        //              "root": "1.2.643.5.1.13.3.25.58.57",
+        //              "extension": "1"
+        //          },
+        //         "familyName": "1",
+        //         "givenName": "1",
+        //         "middleName": "1",
+        //         "snils": "128-971-396 05 ",
+        //         "omsPolicy": {
+        //            "series": "1",
+        //            "type": "\u041e\u041c\u0421",
+        //            "number": "1"
+        //         },
+        //         "identityDocument": {
+        //            "series": "1",
+        //            "type": "01",
+        //            "number": "1"
+        //         }
+        //      }
+        //   }
+        // }
+        logger.info("request patientQuery: " + input);
+
+        //final Map jsonMap = new Gson().fromJson(input, Map.class);
+        final String secureToken = getBinarySecurityToken(input);
+
+        String messageUuid = UUID.randomUUID().toString();
+        QueryIEMKService service = new QueryIEMKService();
+        service.setHandlerResolver(new SdaHandlerResolver(secureToken, messageUuid));
+        QueryIEMKServiceSoap port = service.getQueryIEMKServiceSoap();
+
+        PatientQuery patientQuery = toEhrPatientQuery(input);
+
+        CallbackService.setRestCallback(this);
+        port.patientQuery(patientQuery);
+        synchronized(lockPatientResponse) {
+            try {
+                lockPatientResponse.wait(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+        String resJson = (new Gson()).toJson(patientResponse == null ? CallbackFactory.createPatientResponse() : patientResponse);
+        patientResponse = null;
+        return Response.status(Response.Status.OK).entity(resJson).build();
+    }
+
+    public static PatientQuery toEhrPatientQuery(String input) {
+        PatientQueryInp patientQueryInp = new Gson().fromJson(input, PatientQueryInp.class);
+        PatientQuery patientQuery = EhrFactory.createPatientQuery();
+
+        if (patientQueryInp.data != null) {
+            patientQuery.setFacilityCode(patientQueryInp.data.facilityCode);
+            final PatientQueryInp.PatientQueryData.PatientQueryInitiatedBy initiatedBy = patientQueryInp.data.initiatedBy;
+            if (initiatedBy != null) {
+                final Employee employee = EhrFactory.createEmployee();
+                patientQuery.setInitiatedBy(employee);
+                final PatientQueryInp.PatientQueryData.PatientQueryInitiatedBy.PatientQuerySpeciality speciality = initiatedBy.specialty;
+                if (speciality != null) {
+                    employee.setCode(speciality.code);
+                    employee.setCodingSystem(speciality.codingSystem);
+                    employee.setName(speciality.name);
+                }
+                if (initiatedBy.role != null) {
+                    final CodeAndName codeAndName = EhrFactory.createCodeAndName();
+                    employee.setRole(codeAndName);
+                    codeAndName.setCode(initiatedBy.role.code);
+                    codeAndName.setCodingSystem(initiatedBy.role.codingSystem);
+                    codeAndName.setName(initiatedBy.role.name);
+                }
+                employee.setSnils(initiatedBy.snils.trim());
+                employee.setFamilyName(initiatedBy.familyName);
+                employee.setGivenName(initiatedBy.givenName);
+                employee.setMiddleName(initiatedBy.middleName);
+            }
+            final PatientQueryInp.PatientQueryData.PatientQueryParams params = patientQueryInp.data.params;
+            if (params != null) {
+                final PatientParams patientParams = EhrFactory.createPatientParams();
+                patientQuery.setParams(patientParams);
+                patientParams.setGender(params.gender);
+                //TODO Add to request!
+                //patientParams.setDobHigh(???);
+                //patientParams.setDobLow(???);
+                if (params.mrn != null) {
+                    final InstanceIdentifier instanceIdentifier = EhrFactory.createInstanceIdentifier();
+                    patientParams.setMrn(instanceIdentifier);
+                    instanceIdentifier.setRoot(params.mrn.root);
+                    instanceIdentifier.setExtension(params.mrn.extension);
+                }
+                patientParams.setFamilyName(params.familyName);
+                patientParams.setGivenName(params.givenName);
+                patientParams.setMiddleName(params.middleName);
+                patientParams.setSnils(params.snils.trim());
+                if (params.omsPolicy != null) {
+                    patientParams.setOmsPolicy(doEhrDoc(params.omsPolicy));
+                }
+                if (params.identityDocument != null) {
+                    patientParams.setIdentityDocument(doEhrDoc(params.identityDocument));
+                }
+            }
+        }
+        return patientQuery;
+    }
+
+    private static SeriesNumberAndType doEhrDoc(PatientQueryDoc omsPolicy) {
+        final SeriesNumberAndType seriesNumberAndType = EhrFactory.createSeriesNumberAndType();
+        seriesNumberAndType.setNumber(omsPolicy.number);
+        seriesNumberAndType.setSeries(omsPolicy.series);
+        seriesNumberAndType.setType(omsPolicy.type);
+        return seriesNumberAndType;
+    }
+
+    private String getBinarySecurityToken(String input) {
+        PatientQueryInp patientQueryInp = new Gson().fromJson(input, PatientQueryInp.class);
+        return patientQueryInp.headers.BinarySecurityToken;
+    }
+
+    @POST
+    @Consumes("application/json")
+    @Path("/documentQuery/")
+    public Response documentQuery(String input) {
+        logger.info("request documentQuery: " + input);
+        DocumentQuery documentQuery = new DocumentQuery();
+        DocumentResponse res = new DocumentResponse();
+        String resJson = (new Gson()).toJson(res);
+        return Response.status(Response.Status.OK).entity(resJson).build();
+    }
+
+
+    @POST
+    @Consumes("application/json")
+    @Path("/retrieveDocumentQuery/")
+    public Response retrieveDocumentQuery(String input) {
+        logger.info("request retrieveDocumentQuery: " + input);
+        DocumentQuery documentQuery = new DocumentQuery();
+        CallbackService.setRestCallback(this);
+        RetrieveDocumentResponse res = new RetrieveDocumentResponse();
+        String resJson = (new Gson()).toJson(res);
+        return Response.status(Response.Status.OK).entity(resJson).build();
+    }
+
+    @Override
+    public void containerResponse(BaseResponse parameters) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void patientQueryResponse(PatientResponse parameters) {
+        synchronized(this) {
+            patientResponse = parameters;
+            lockPatientResponse.notify();
+        }
+    }
+
+    @Override
+    public void documentQueryResponse(DocumentResponse parameters) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void retrieveDocumentQueryResponse(RetrieveDocumentResponse parameters) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+}
