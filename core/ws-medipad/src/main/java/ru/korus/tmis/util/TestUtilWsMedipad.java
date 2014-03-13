fc12a91e1ccc15b8d0123923fcaf9a4763b900f5
MarmaladeSky <alexey.kislin@gmail.com>
2014-03-13 15:48:18 +0400

    remove jersey dependency: Remove redundant class

--- core/ws-medipad/src/main/java/ru/korus/tmis/util/TestUtilWsMedipad.java
+++ core/ws-medipad/src/main/java/ru/korus/tmis/util/TestUtilWsMedipad.java
@@ -23,7 +23,7 @@ public class TestUtilWsMedipad implements TestUtil {
         final Package[] res = new Package[]{
                 ru.korus.tmis.ws.medipad.AuthenticationWebService.class.getPackage(),
                 ru.korus.tmis.ws.webmis.rest.AppealRegistryRESTImpl.class.getPackage(),
-                ru.korus.tmis.ws.webmis.rest.servlet.RestServlet.class.getPackage(),
+                ru.korus.tmis.ws.webmis.rest.servlet.AuthenticationFilter.class.getPackage(),
                 ru.korus.tmis.ws.webmis.rest.interceptors.ExceptionJSONMessage.class.getPackage(),
                 ru.korus.tmis.ws.handlers.AuthenticatingHandler.class.getPackage(),
                 ru.korus.tmis.ws.impl.AuthenticationWSImpl.class.getPackage()
