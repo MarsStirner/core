e83b894058b482aa263b125824c64c9dd6025042
szagrebelny <szgrebelny@korusconsulting.ru>
2014-03-13 17:10:18 +0400

    update testCreateAction

--- core/ws-medipad/src/test/resources/sql/init.sql
+++ core/ws-medipad/src/test/resources/sql/init.sql
@@ -6,18 +6,18 @@ VALUES
 SET @last = LAST_INSERT_ID();
 INSERT INTO Person_Profiles (userProfile_id, person_id) VALUES (29, @last);
 
-INSERT INTO `rbbloodcomponenttype` (`code`, `name`) VALUES ('02.02.003', 'Плазма свежезамороженная, полученная автоматическим аферезом');
-INSERT INTO `rbbloodcomponenttype` (`code`, `name`) VALUES ('02.01.020', 'Тромбоцитный концентрат, полученный автоматическим аферезом');
-INSERT INTO `rbbloodcomponenttype` (`code`, `name`) VALUES ('01.01.010', 'Эритроцитная взвесь с ресуспендирующим раствором, фильтрованная');
-INSERT INTO `rbbloodcomponenttype` (`code`, `name`) VALUES ('01.03.001', 'Кровь консервированная');
-INSERT INTO `rbbloodcomponenttype` (`code`, `name`) VALUES ('09.01.001', 'Лейкотромбомасса с фильтром');
-INSERT INTO `rbbloodcomponenttype` (`code`, `name`) VALUES ('02.02.001', 'Плазма свежезамороженная из дозы крови');
-INSERT INTO `rbbloodcomponenttype` (`code`, `name`) VALUES ('02.02.008', 'Плазма свежезамороженная из дозы крови    Карантинизовано 6 месяцев');
-INSERT INTO `rbbloodcomponenttype` (`code`, `name`) VALUES ('01.01.005', 'Эритроцитная масса с удаленным лейкотромбослоем');
-INSERT INTO `rbbloodcomponenttype` (`code`, `name`) VALUES ('01.01.002', 'Эритроцитная масса, фильтрованная');
-INSERT INTO `rbbloodcomponenttype` (`code`, `name`) VALUES ('01.02.002', 'Аутоэритроцитная масса, фильтрованная');
-INSERT INTO `rbbloodcomponenttype` (`code`, `name`) VALUES ('01.01.009', 'Эритроцитная взвесь с ресуспендирующим раствором');
-INSERT INTO `rbbloodcomponenttype` (`code`, `name`) VALUES ('02.03.001', 'Аутоплазма свежезамороженная из дозы крови');
-INSERT INTO `rbbloodcomponenttype` (`code`, `name`) VALUES ('01.01.014', 'Эритроцитарная взвесь, лейкофильтрованная, полученная автоматическим аферезом');
-INSERT INTO `rbbloodcomponenttype` (`code`, `name`) VALUES ('03.01.006', 'Лейкоцитный концентрат');
+INSERT INTO `rbBloodComponentType` (`code`, `name`) VALUES ('02.02.003', 'Плазма свежезамороженная, полученная автоматическим аферезом');
+INSERT INTO `rbBloodComponentType` (`code`, `name`) VALUES ('02.01.020', 'Тромбоцитный концентрат, полученный автоматическим аферезом');
+INSERT INTO `rbBloodComponentType` (`code`, `name`) VALUES ('01.01.010', 'Эритроцитная взвесь с ресуспендирующим раствором, фильтрованная');
+INSERT INTO `rbBloodComponentType` (`code`, `name`) VALUES ('01.03.001', 'Кровь консервированная');
+INSERT INTO `rbBloodComponentType` (`code`, `name`) VALUES ('09.01.001', 'Лейкотромбомасса с фильтром');
+INSERT INTO `rbBloodComponentType` (`code`, `name`) VALUES ('02.02.001', 'Плазма свежезамороженная из дозы крови');
+INSERT INTO `rbBloodComponentType` (`code`, `name`) VALUES ('02.02.008', 'Плазма свежезамороженная из дозы крови    Карантинизовано 6 месяцев');
+INSERT INTO `rbBloodComponentType` (`code`, `name`) VALUES ('01.01.005', 'Эритроцитная масса с удаленным лейкотромбослоем');
+INSERT INTO `rbBloodComponentType` (`code`, `name`) VALUES ('01.01.002', 'Эритроцитная масса, фильтрованная');
+INSERT INTO `rbBloodComponentType` (`code`, `name`) VALUES ('01.02.002', 'Аутоэритроцитная масса, фильтрованная');
+INSERT INTO `rbBloodComponentType` (`code`, `name`) VALUES ('01.01.009', 'Эритроцитная взвесь с ресуспендирующим раствором');
+INSERT INTO `rbBloodComponentType` (`code`, `name`) VALUES ('02.03.001', 'Аутоплазма свежезамороженная из дозы крови');
+INSERT INTO `rbBloodComponentType` (`code`, `name`) VALUES ('01.01.014', 'Эритроцитарная взвесь, лейкофильтрованная, полученная автоматическим аферезом');
+INSERT INTO `rbBloodComponentType` (`code`, `name`) VALUES ('03.01.006', 'Лейкоцитный концентрат');
 
