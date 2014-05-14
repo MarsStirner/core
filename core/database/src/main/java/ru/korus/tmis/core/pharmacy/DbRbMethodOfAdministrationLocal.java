dd7bec4f2b57247320fd8af0117d57d5fb9f1cf4
MarmaladeSky <alexey.kislin@gmail.com>
2014-05-14 14:32:04 +0400

    remove unused

--- core/database/src/main/java/ru/korus/tmis/core/pharmacy/DbRbMethodOfAdministrationLocal.java
+++ core/database/src/main/java/ru/korus/tmis/core/pharmacy/DbRbMethodOfAdministrationLocal.java
@@ -1,14 +1,8 @@
 package ru.korus.tmis.core.pharmacy;
 
-import org.joda.time.DateTime;
-import ru.korus.tmis.core.entity.model.Action;
 import ru.korus.tmis.core.entity.model.RbMethodOfAdministration;
-import ru.korus.tmis.core.entity.model.RlsNomen;
-import ru.korus.tmis.core.entity.model.pharmacy.Pharmacy;
-import ru.korus.tmis.core.exception.CoreException;
 
 import javax.ejb.Local;
-import java.util.List;
 
 /**
  * Author:      Sergey A. Zagrebelny <br>
