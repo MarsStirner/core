74166748913d919ec653035d3f2e8a465e1e0238
szagrebelny <szgrebelny@korusconsulting.ru>
2014-06-27 11:43:20 +0400

    add admin-panel

---
+++ core/admin-panel/src/main/webapp/VAADIN/widgetsets/ru.korus.tmis.admin.AppWidgetSet/deferredjs/8BFC98ACABC922A785EFBB20ECD85A6B/1.cache.js
@@ -0,0 +1,1 @@
+$wnd.ru_korus_tmis_admin_AppWidgetSet.runAsyncCallback1('h8(1049,1,qnd);_.nc=function d$b(){xPb((!sPb&&(sPb=new yPb),sPb),this.b.e)};zbd(Vh)(1);\n//# sourceURL=ru.korus.tmis.admin.AppWidgetSet-1.js\n')
