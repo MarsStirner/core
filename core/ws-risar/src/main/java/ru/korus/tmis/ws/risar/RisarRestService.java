09ec76c29b081c67a6aaf8f616f76bab5e49ac18
szagrebelny <szgrebelny@korusconsulting.ru>
2014-09-17 16:22:50 +0400

    RISAR: find patient by personal info

--- core/ws-risar/src/main/java/ru/korus/tmis/ws/risar/RisarRestService.java
+++ core/ws-risar/src/main/java/ru/korus/tmis/ws/risar/RisarRestService.java
@@ -22,10 +22,7 @@ import javax.ws.rs.core.Response;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Author:      Sergey A. Zagrebelny <br>
@@ -67,13 +64,17 @@ public class RisarRestService {
                     em.createNamedQuery("ClientIdentification.findByPatientAndSystem").
                             setParameter("clientId", clientId).setParameter("code", "rs").setMaxResults(100).getResultList();
             logger.info("RISAR notification. RISAR identification count: " + clientIdentificationList.size());
+            if (clientIdentificationList.isEmpty()) {
+                clientIdentificationList.addAll(findByPatientInfo(action.getEvent().getPatient()));
+            }
             for (ClientIdentification clientIdentification : clientIdentificationList) {
                 logger.info("RISAR notification. RISAR identifier: " + clientIdentification.getIdentifier());
                 sendExamToRisar(action, clientIdentification);
             }
+
         } catch (CoreException ex) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
-        }  catch (Exception ex) {
+        } catch (Exception ex) {
             StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
             ex.printStackTrace(pw);
@@ -82,6 +83,74 @@ public class RisarRestService {
         }
 
         return Response.ok().build();
+    }
+
+    private List<ClientIdentification> findByPatientInfo(Patient patient) throws CoreException {
+
+        RestTemplate rest = new RestTemplate();
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+        map.add("patientFIO[name]", patient.getFirstName());
+        map.add("patientFIO[surname]", patient.getLastName());
+        map.add("patientFIO[middlename]", patient.getPatrName());
+        Date birthDate = patient.getBirthDate();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        if (birthDate != null) {
+            String birthDateStr = simpleDateFormat.format(birthDate);
+            map.add("birthDate:", birthDateStr);
+        }
+        int index = 0;
+        for (ClientDocument doc : patient.getClientDocuments()) {
+            String codeType = doc.getDocumentType() == null ? null : doc.getDocumentType().getCode();
+            addToRequest("codeType", codeType, index, map);
+            addToRequest("number", doc.getNumber(), index, map);
+            addToRequest("series", doc.getSerial(), index, map);
+            Date createDate = doc.getDate();
+            if (createDate != null) {
+                String createDateStr = simpleDateFormat.format(createDate);
+                if( !createDateStr.contains("1800")) {
+                    addToRequest("registry_date", createDateStr, index, map);
+                }
+            }
+            ++index;
+        }
+        logger.info("RISAR notification. Find patient param: " + map);
+        final String url = ConfigManager.Risar().ServiceUrl() + "/api/patients/v2/find/";
+        logger.info("RISAR notification. Find patientt url: " + url);
+        RisarResponse result = rest.postForObject(url, map, RisarResponse.class);
+        logger.info("RISAR notification. Find result: " + result);
+        if (!result.isOk()) {
+            throw new CoreException("RISAR find patient error:" + result);
+        }
+
+        List<ClientIdentification> res = new LinkedList<ClientIdentification>();
+        res.add(createNewIdentification(patient, result));
+        return res;
+    }
+
+    private ClientIdentification createNewIdentification(Patient patient, RisarResponse result) {
+        final ClientIdentification identification = new ClientIdentification();
+        identification.setClient(patient);
+        identification.setIdentifier(result.getPatientId());
+        identification.setCreateDatetime(new Date());
+        identification.setModifyDatetime(new Date());
+        identification.setDeleted(false);
+        identification.setCheckDate(new Date());
+        final AccountingSystem accountingSystem = findOrCreateAccountingSystem();
+        identification.setAccountingSystem(accountingSystem);
+        em.persist(identification);
+        return identification;
+    }
+
+    private AccountingSystem findOrCreateAccountingSystem() {
+        List<AccountingSystem> res = em.createQuery("SELECT accountingSystem FROM AccountingSystem accountingSystem WHERE accountingSystem.code = 'rs'", AccountingSystem.class).
+                setMaxResults(100).getResultList();
+        return res.iterator().next();
+    }
+
+    private void addToRequest(String param, String value, int index, MultiValueMap<String, String> map) {
+        if (value != null && !value.isEmpty()) {
+            map.add("patientDocuments[" + index + "]" + "[" + param + "]", value);
+        }
     }
 
     private void sendExamToRisar(Action action, ClientIdentification clientIdentification) throws CoreException {
