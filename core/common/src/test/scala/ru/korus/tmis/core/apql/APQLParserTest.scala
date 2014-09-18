b07cb4ab17f65ba1de1f4bbea3bae5f669847b92
MarmaladeSky <alexey.kislin@gmail.com>
2014-09-18 14:22:11 +0400

    apql: tests modification

--- core/common/src/test/scala/ru/korus/tmis/core/apql/APQLParserTest.scala
+++ core/common/src/test/scala/ru/korus/tmis/core/apql/APQLParserTest.scala
@@ -1,13 +1,14 @@
 package ru.korus.tmis.core.apql
 
-import org.junit.Test
+import org.scalatest.Assertions
+import org.testng.annotations.Test
 
 /**
  * Author: <a href="mailto:alexey.kislin@gmail.com">Alexey Kislin</a>
  * Date: 9/15/14
  * Time: 2:41 PM
  */
-class APQLParserTest {
+class APQLParserTest extends Assertions {
 
   @Test def parseCondition() = parse(
     """IF (getActionsByEvent(12, "typeCode").first().properties().containsValueOf("propertyCode"))
@@ -30,7 +31,7 @@ class APQLParserTest {
   private def parse(s: String): Unit = {
     val p = new APQLParser
     p.parse(s) match {
-      case x: p.NoSuccess => assert(assertion = false, "\n" + x)
+      case x: p.NoSuccess => fail("\n" + x)
       case x: AnyRef => System.out.println(x)
     }
   }
