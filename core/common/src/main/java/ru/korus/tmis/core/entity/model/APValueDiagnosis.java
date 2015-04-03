5a909001e3e6ecd60eb3fc25860881a31af63612
SergeyKorus <szgrebelny@korusconsulting.ru>
2015-04-03 18:52:06 +0400

    update ActionProperty

---
+++ core/common/src/main/java/ru/korus/tmis/core/entity/model/APValueDiagnosis.java
@@ -0,0 +1,10 @@
+package ru.korus.tmis.core.entity.model;
+
+/**
+ * Author:      Sergey A. Zagrebelny <br>
+ * Date:        31.03.2015, 20:05 <br>
+ * Company:     Korus Consulting IT<br>
+ * Description:  <br>
+ */
+public class APValueDiagnosis {
+}
