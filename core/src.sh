7f26766341c84382353fb4e8766e4ce31e9e7e0c
SergeyKorus <szgrebelny@korusconsulting.ru>
2015-07-17 13:34:33 +0300

    src prepare script

--- core/src.sh
+++ core/src.sh
@@ -1,11 +1,12 @@
+version=2.6.0-SNAPSHOT
 mvn source:aggregate
 rm -rf ./target/src
 mkdir ./target/src
-cp ./target/tmis-core-2.6.0-SNAPSHOT-sources.jar  ./target/src/tmis-core-2.6.0-SNAPSHOT-sources.jar 
+cp ./target/tmis-core-${version}-sources.jar  ./target/src/tmis-core-${version}-sources.jar
 cd ./target/src
-"${JAVA_HOME}"/bin/jar xf ./tmis-core-2.6.0-SNAPSHOT-sources.jar 
+"${JAVA_HOME}"/bin/jar xf ./tmis-core-2.6.0-SNAPSHOT-sources.jar
 cd ../../
-rm ./target/src/tmis-core-2.6.0-SNAPSHOT-sources.jar 
-rm ./target/tmis-core-2.6.0-SNAPSHOT-sources.jar 
+rm ./target/src/tmis-core-${version}-sources.jar
+rm ./target/tmis-core-${version}-sources.jar
 cp -r ./*/src/main/scala/* ./target/src
 mvn dependency:list | sort | uniq  | grep "\[INFO\]    " > ./target/dependency.txt 
