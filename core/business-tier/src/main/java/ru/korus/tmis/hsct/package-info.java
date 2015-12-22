a210ec0ea55782efebd1a56b83515304908ef66b
Upatov Egor <EUpatov@korusconsulting.ru>
2015-12-22 20:00:44 +0300

    new branch initial commit

---
+++ core/business-tier/src/main/java/ru/korus/tmis/hsct/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Author: Upatov Egor <br>
+ * Date: 22.12.2015, 20:56 <br>
+ * Company: hitsl (Hi-Tech Solutions) <br>
+ * Description: Package для работы с ТГСК (Трансплантация Гемопоэтических Стволовых Клеток ) <br>
+ */
+package ru.korus.tmis.hsct;
